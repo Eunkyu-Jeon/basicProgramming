1c470d3886c50defea448e224decab5632e500c7
Eunkyu-Jeon <98139109+Eunkyu-Jeon@users.noreply.github.com>
2022-03-18 10:44:43 +0900

    Add files via upload
    구구단

---
+++ ConsoleApplication1.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 2; i < 10; i++) {
+		
+		for (int k = 1; k < 10; k++) {
+			printf("%dx%d=%d ", i, k, k * i);
+			if (k == 9) {
+				printf("\n");
+			}
+		}
+	}
+}
