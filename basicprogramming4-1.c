34b956000f12ccfbce135bb507522fa55b95c392
Eunkyu-Jeon <98139109+Eunkyu-Jeon@users.noreply.github.com>
2022-03-29 21:43:41 +0900

    Add files via upload

---
+++ basicprogramming4-1.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+    int a; 
+    int b; //두 수를 int 형으로 선언
+    int sum, min, mul; //계산의 결과값을 int 형으로 선언한다.
+    printf("input number a,b : ");
+    scanf_s("%d %d", &a, &b); //scanf_s를 사용해서 두 수를 입력받는다.
+
+    sum = a + b;
+    min = a - b;
+    mul = a * b;
+
+    printf("%d + %d = %d\n", a, b, sum);
+    printf("%d - %d = %d\n", a, b, min);
+    printf("%d * %d = %d\n", a, b, mul); //printf를 통하여 출력
+
+}
---
+++ basicprogramming4-2.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(void) {
+    int a = INT_MAX;//int형 최대값
+    int b = INT_MIN;//int형 최소값
+    printf("%d,%d\n", a, b);
+
+    int c = CHAR_MAX;//char형 최대값
+    int d = CHAR_MIN;//char형 최소값
+    printf("%d,%d\n", c, d);
+
+    double e = DBL_MAX;//double형 최대값
+    double f = DBL_MIN;//double형 최소값
+    printf("%e,%e\n", e, f); //수가 크기때문에 지수형태로 출력
+
+    float g = FLT_MAX;//float형 최대값
+    float h = FLT_MIN;//float형 최소값
+    printf("%e,%e\n", g, h);
+
+}
